e8789925749dc9e55c206ab8f43526ee4cab3dea
Livio Brunner <livio.brunner.lb1@gmail.com>
2018-01-13 14:21:00 +0100

    Add boostrap and index site

---
+++ M183-Blog/ViewModels/HomeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using M183_Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace M183_Blog.ViewModels
+{
+    public class HomeViewModel
+    {
+        List<Post> posts;
+        public HomeViewModel(List<Post> posts)
+        {
+            this.posts = posts;
+        }
+
+        public List<Post> Posts
+        {
+            get
+            {
+                return this.posts;
+            }
+        }
+    }
+}
